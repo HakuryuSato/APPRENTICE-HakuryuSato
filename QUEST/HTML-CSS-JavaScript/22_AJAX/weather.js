39f9cbf2474669756442783ef28f04081b593179
HakuryuSato <r700540@gmail.com>
2024-08-09 19:20:20 +0900

    更新

--- QUEST/HTML-CSS-JavaScript/22_AJAX/weather.js
+++ QUEST/HTML-CSS-JavaScript/22_AJAX/weather.js
@@ -1,12 +1,30 @@
-const doc = document
-const weatherForm = doc.getElementById("weatherForm")
-const weatherResult = doc.getElementById("weatherResult")
+require("dotenv").config();
+const apiKey = process.env.API_KEY;
 
-doc.addEventListener("submit",function(event){
+const doc = document;
+const weatherForm = doc.getElementById("weatherForm");
+const weatherResult = doc.getElementById("weatherResult");
+
+doc.addEventListener("submit", function (event) {
     event.preventDefault;
-    const latitude = weatherForm.getElementById("latitude")
-    const longitude = weatherForm.getElementById("longitude")
+    const latitude = weatherForm.getElementById("latitude");
+    const longitude = weatherForm.getElementById("longitude");
+    const url =
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=ja&appid=${apiKey}`;
 
-    
-    console.log(latitude,longitude)
-})
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const temperature = data.main.temp;
+            const weatherDescription = data.weather[0].description;
+            document.getElementById("weatherResult").innerHTML = `
+            <p>気温: ${temperature} °C</p>
+            <p>天気: ${weatherDescription}</p>
+        `;
+        })
+        .catch((error) => {
+            document.getElementById("weatherResult").innerHTML =
+                `<p>天気情報の取得に失敗しました。</p>`;
+            console.error("Error:", error);
+        });
+});
